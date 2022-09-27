e7ac6acfc14eb053559ef0684288b67aea71b8f3
jomulagy <jomulagy688@gmail.com>
2022-09-27 12:23:25 +0900

    메인화면에 사진에 링크 달기

--- clothe_management/account/views.py
+++ clothe_management/account/views.py
@@ -14,28 +14,29 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 import string
 import random
 import time
 
 img_urls=[]
-
+page_urls=[]
 def loading(request):
     return render(request,'loading.html')
 
 def load_img(request):
 
     options = webdriver.ChromeOptions() # 크롬 옵션 객체 생성
-    options.add_argument('headless') # headless 모드 설정
-    options.add_argument("window-size=1920x1080") # 화면크기(전체화면)
-    options.add_argument("disable-gpu") 
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
+    # options.add_argument('headless') # headless 모드 설정
+    # options.add_argument("window-size=1920x1080") # 화면크기(전체화면)
+    # options.add_argument("disable-gpu") 
+    # options.add_argument("disable-infobars")
+    # options.add_argument("--disable-extensions")
 
-    # 속도 향상을 위한 옵션 해제
-    prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
-    options.add_experimental_option('prefs', prefs)
+    # # 속도 향상을 위한 옵션 해제
+    # prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
+    # options.add_experimental_option('prefs', prefs)
 
 
     chromedriver_autoinstaller.install()
@@ -45,16 +46,24 @@ def load_img(request):
     driver.implicitly_wait(1)
     
     driver.get('https://www.hiver.co.kr/')
-
-    for i in range(1,10):
-        for j in range(10000000000):
-            img=driver.find_element(By.XPATH,"//*[@id='adaptive']/div/ul/li["+str(i)+"]/img")
-            if img.get_attribute("src")!=None:
-                driver.find_element(By.XPATH,"//*[@id='adaptive']/div/button[2]").click()
-                break
+    WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//*[@id='adaptive']/div/div/button[2]")))
+    driver.find_element(By.XPATH,"//*[@id='adaptive']/div/div/button[2]").click()
+    for i in range(3,13):
+        
+        WebDriverWait(driver,10).until(EC.presence_of_element_located((By.XPATH,"//*[@id='adaptive']/div/ul/li["+str(i)+"]/img")))
+        img = driver.find_element(By.XPATH,"//*[@id='adaptive']/div/ul/li["+str(i)+"]/img")
         img_urls.append(img.get_attribute("src"))
+        print(img.get_attribute("src"))
+        driver.find_element(By.XPATH,"//*[@id='adaptive']/div").click()
+        page_urls.append(driver.current_url)
+        print(driver.current_url)
+        time.sleep(1)
+        driver.back()
 
+        driver.find_element(By.XPATH,"//*[@id='adaptive']/div/button[2]").click()
+        time.sleep(1)
     driver.quit()
+
     return redirect(home)
 
 
@@ -70,20 +79,20 @@ def register(request):
             new_user.email = new_user.username
             new_user.save()
 
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            auth_login(request, user)  # 로그인
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserForm()
-    return render(request, "register/register.html", {'form': form})
+        return render(request, "register/register.html", {'form': form})
 
 def login(request):
     return render(request,"login/login.html")
 
 def home(request):
-    return render(request, 'index.html',{"img_urls":img_urls})
+    urls = list(zip(img_urls, page_urls))
+    context = {
+        "urls" : urls,
+    }
+    return render(request, 'index.html',context)
 
 def forgot_password(request):
     context = {}
