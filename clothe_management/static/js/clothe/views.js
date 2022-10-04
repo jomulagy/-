bb2d8bdbccf84b59400eb2ee11b40ede2e6c2057
kimyeonchul <yeonchul0807@nate.com>
2022-10-04 16:46:06 +0900

    탈퇴하기 백앤드랑 연결

--- clothe_management/static/js/clothe/views.js
+++ clothe_management/static/js/clothe/views.js
@@ -1,46 +1,52 @@
-var img_url =[]
-var clothe_id =[]
+var img_url = []
+var clothe_id = []
 
 var page_num = 0
 var total_page_num
 
-$(document).ready(function(){
-    if (clothe_id=='[]'){
+$(document).ready(function () {
+    if (clothe_id == '[]') {
         img_url = []
         clothe_id = []
     }
-    else{
+    else {
+        console.log(img_url);
+        console.log(image_urls);
         image_urls = image_urls.split(",")
-        for(var url of image_urls){
-            img_url.push(url.substring(url.indexOf("&#x27;")+6,url.lastIndexOf("&#x27;")))
+        for (var url of image_urls) {
+            img_url.push(url.substring(url.indexOf("&#x27;") + 6, url.lastIndexOf("&#x27;")))
         }
+        console.log(img_url);
+        console.log(image_urls);
 
-        clothe_ids = clothe_ids.substring(1,clothe_ids.length-1)
+
+        clothe_ids = clothe_ids.substring(1, clothe_ids.length - 1)
         clothe_ids = clothe_ids.split(", ")
 
-        for(var id of clothe_ids){
+        for (var id of clothe_ids) {
             clothe_id.push(id)
         }
     }
 
+    
     print_clothes(page_num)
     print_pagenations()
 
-    $('.page-item').click(function(){
+    $('.page-item').click(function () {
         $("#card-area").empty();
-    
+
         var request_page = $(this).text();
-        if(request_page == 'Previous'){
-            request_page=parseInt(page_num)-1
+        if (request_page == 'Previous') {
+            request_page = parseInt(page_num) - 1
         }
 
-        else if(request_page == 'Next'){
-            request_page = parseInt(page_num)+1
+        else if (request_page == 'Next') {
+            request_page = parseInt(page_num) + 1
         }
 
         $(".pagination>.page-item:first").removeClass("disabled");
         $(".pagination>.page-item:last").removeClass("disabled");
-    
+
         if (request_page == 1) {
             $(".pagination>.page-item:first").addClass("disabled");
         }
@@ -49,67 +55,71 @@ $(document).ready(function(){
         }
         $(".pagination>.page-item").eq(page_num).removeClass("active");
         $(".pagination>.page-item").eq(request_page).addClass("active");
-        if (img_url.length != 0){
-            print_clothes(request_page-1);
+        if (img_url.length != 0) {
+            print_clothes(request_page - 1);
         }
         page_num = request_page;
-    
+
     });
 
     $('.pagination>.page-item:nth-child(2)').trigger("click");
 });
 
-function print_pagenations(){  
+function print_pagenations() {
     var target = $('.pagination>.page-item:first-child')
-    if (img_url.length%20!=0){
-        total_page_num = parseInt(img_url.length/20)+1
+    if (img_url.length % 20 != 0) {
+        total_page_num = parseInt(img_url.length / 20) + 1
     }
-    else{
-        total_page_num = parseInt(img_url.length/20)
+    else {
+        total_page_num = parseInt(img_url.length / 20)
     }
-    if(total_page_num==0){
+    if (total_page_num == 0) {
         total_page_num = 1
     }
-    for(var i = 0;i<total_page_num;i++){
+    for (var i = 0; i < total_page_num; i++) {
         var page_item = document.createElement('li');
         $(page_item).addClass('page-item');
         $(target).after(page_item)
         target = $(page_item);
+        
 
         var page_link = document.createElement('a');
         $(page_link).addClass('page-link');
-        $(page_link).text(i+1);
+        $(page_link).text(i + 1);
         page_item.append(page_link);
     }
-    
+
 }
-function print_clothes(page_num){
+function print_clothes(page_num) {
     var container = $("#card-area")
-    var start = 20*page_num;
-    var end = (img_url.length<20*(page_num+1)-1)?img_url.length:20*(page_num+1)-1
-    
-    for(var i = start; i<end;i++){
-        if(i%4==0){
-            var row = document.createElement('div');
-            $(row).addClass('row row-cols-4')
-            container.append(row)
+    var start = 20 * page_num;
+    var end = (img_url.length < 20 * (page_num + 1) - 1) ? img_url.length : 20 * (page_num + 1) - 1
+
+    console.log(img_url.length);
+
+        for (var i = start; i < end; i++) {
+            if (i % 4 == 0) {
+                var row = document.createElement('div');
+                $(row).addClass('row row-cols-4')
+                container.append(row)
+            }
+            var col = document.createElement('div');
+            $(col).addClass('col')
+            row.append(col)
+
+            var card = document.createElement('div');
+            $(card).addClass('card')
+            col.append(card)
+
+            var a = document.createElement('a');
+            $(a).attr("href", "/clothe/detail/" + clothe_id[i])
+            card.append(a)
+
+            var img = document.createElement('img');
+            $(img).addClass('card-img-top');
+            $(img).attr("src", img_url[i]);
+            a.append(img);
         }
-        var col = document.createElement('div');
-        $(col).addClass('col')
-        row.append(col)
-
-        var card = document.createElement('div');
-        $(card).addClass('card')
-        col.append(card)
-        
-        var a = document.createElement('a');
-        $(a).attr("href","/clothe/detail/"+clothe_id[i])
-        card.append(a)
-
-        var img = document.createElement('img');
-        $(img).addClass('card-img-top');
-        $(img).attr("src",img_url[i]);
-        a.append(img);
     }
-}
+
 
--- clothe_management/static/js/secession.js
+++ clothe_management/static/js/secession.js
@@ -1,10 +1,58 @@
 
 $('.secession_btn').click(function () {
-    let userid = $(".userId").val();
-    let userpwd = $(".userPwd").val();
-    checkUser(userid,userpwd);
+    let pwd = $('.userPwd').val();
+    secession_user(user_id, pwd);
 });
 
-function checkUser(userid,userpwd){
-   console.log(userid,userpwd);
+$('.userPwd').on("keyup",function(key){
+    if(key.keyCode==13){
+        let pwd = $('.userPwd').val();
+    secession_user(user_id, pwd);
+    }
+});
+
+$('.close-btn').click(function () {
+    $('.modal-wrap').css('display', 'none');
+    $('.modal').css('display', 'none');
+});
+
+$('#modal_return_btn').click(function () {
+    $('.modal-wrap').css('display', 'none');
+    $('.modal').css('display', 'none');
+});
+
+$('#modal_secession_btn').click(function () {
+
+});
+
+function secession_user($userid, $pwd) {
+    $.ajax({
+        //요청이 전송될 URL 주소
+        url: 'http://127.0.0.1:8000/check_pwd/',
+        type: "POST",
+        dataType: "JSON",
+        data: JSON.stringify({
+            "id": $userid,
+            "pwd": $pwd
+        }),
+        headers: { "X-CSRFToken": "{{ csrf_token }}" },
+
+        success: function (data) {
+            if (data.result == true && data.error=="") {
+                $('.modal-wrap').css('display', 'block');
+                $('.modal').css('display', 'block');
+            }
+            else {
+                alert(data.error);
+            }
+        },
+        error: function (xhr, textStatus, thrownError) {
+            alert(
+                "Could not send URL to Django. Error: " +
+                xhr.status +
+                ": " +
+                xhr.responseText
+            );
+        },
+    });
 }
